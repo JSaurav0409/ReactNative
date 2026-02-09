372d21530144cdf42e5d389b28095ba4330c1be1
Saurav Jha <your-email@example.com>
2026-02-09 20:06:46 +0530

    Remove broken submodule entries for projects 6 and 11

--- CurrencyConverter06/src/Index.d.ts
+++
@@ -1,8 +0,0 @@
-// Creating interface which stores the hardcoded exchange rate and flags image and currency symbol
-
-interface Currency {
-  name: string;
-  value: number;
-  flag: string;
-  symbol: string;
-}
